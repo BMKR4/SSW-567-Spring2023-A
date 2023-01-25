5ab1ac2d5f5a95608bd647ed491a84827716e153
BMKR4 <96380051+BMKR4@users.noreply.github.com>
2023-01-24 21:39:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def my_brand(ass_name):
 
 class TestMyBrand(unittest.TestCase):
     def test_my_brand(self):
-        self.assertEqual(my_brand("HW1","=*=*=*= Manoj Kumar Reddy Buthukuri =*=*=*="+"\n"+"=*=*=*= Course 2023S-SSW567-WS =*=*=*= "+"\n"+"=*=*=*= HW1 =*=*=*= " +"\n"+"=*=*=*= "+str(datetime.datetime.now())+" =*=*=*= "))
+        self.assertEqual(my_brand("HW1"),"=*=*=*= Manoj Kumar Reddy Buthukuri =*=*=*="+"\n"+"=*=*=*= Course 2023S-SSW567-WS =*=*=*= "+"\n"+"=*=*=*= HW1 =*=*=*= " +"\n"+"=*=*=*= "+str(datetime.datetime.now())+" =*=*=*= ")
 
 
 unittest.main(argv=[''], verbosity=1, exit=False);
